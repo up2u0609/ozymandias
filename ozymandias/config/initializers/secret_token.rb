b8cb60913fa0275dbafa37442d0fee81cd0b4c71
up2u0609 <up2u0609@gmail.com>
2011-01-28 17:42:35 +0800

    initialize ozymandias

---
+++ ozymandias/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ozymandias::Application.config.secret_token = 'fe206b1f30dec83d5bd7b8ab0b06ad0a35979f2de45dfc8b843e4b70fb4a3587f527b70ea21cf3bbce5205e07fea7385d46c132422e04c2185c8ad14629c4ea8'
