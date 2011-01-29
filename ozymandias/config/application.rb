01eff47c49a48290af1825de114cba770a1bcb7b
up2u0609 <up2u0609@gmail.com>
2011-01-29 19:59:33 +0800

    add codes

--- ozymandias/config/application.rb
+++ ozymandias/config/application.rb
@@ -1,7 +1,5 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
@@ -38,5 +36,6 @@ module Ozymandias
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.autoload_paths += %W(#{config.root}/lib/scheduled_jobs)
   end
 end
--- ozymandias/config/routes.rb
+++ ozymandias/config/routes.rb
@@ -55,4 +55,8 @@ Ozymandias::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  resources :sites
+  resources :site_tracks do
+    get 'show'
+  end
 end
